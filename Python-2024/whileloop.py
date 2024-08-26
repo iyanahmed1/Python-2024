953c96d7523b5cfe9a98c31195d646885d04afd7
iyanahmed1 <malumasaid123@gmail.com>
2024-08-26 18:54:50 +0300

    Add files via upload

---
+++ Python-2024/whileloop.py
@@ -0,0 +1,29 @@
+#while loop implements repeated code based on a given boolean condition
+#the code written within the while loop will evacuate as long as the while statement evaluates 
+#num1=5
+#while num1<8:
+#   print(num1)
+#    num1+=2 #increment num1 by 1
+
+#create a downtown timer of a rocket launcher
+#import time
+#start=int(input('Enter countdown duration in seconds: '))
+#print(f'Blast off{start} seconds:')
+
+#count=start
+#while count>=0:
+#    time.sleep(1)#pause system for 1 second
+#    print(f'T-{count}')
+#    count-=1
+#time.sleep(0.25)
+#print ('KABOOM!!!')
+
+#Innfitiate count to =0
+#output 'the count is:plus the current value of the count as long
+#as count value is less than or equal to 10
+#print out Exiting the system, Goodbye!! when count is 10
+count= 0
+while(count<10):
+    print('the count is: ',count)
+    count=count+1
+print('Exiting the system. Goodbye!!!')
